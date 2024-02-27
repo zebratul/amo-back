b2d47d71781be6d886f591688ab0e53127ca8a54
zebratul <102689746+zebratul@users.noreply.github.com>
2024-02-27 16:40:01 +0300

    cors fix

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ const sendEmail = require('./helpers/mailer');
 const createContact = require('./helpers/amoCRM');
 
 // Middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allows all origins
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
